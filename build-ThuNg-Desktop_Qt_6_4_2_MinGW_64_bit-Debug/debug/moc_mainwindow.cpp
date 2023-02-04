9d4f8f91d7452d483477b676e737350c38693b42
LeTuan-iu-HKN <letuanhf@gmail.com>
2023-02-04 14:19:17 +0700

    Ahihi do ngok

---
+++ build-ThuNg-Desktop_Qt_6_4_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,273 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../ThuNg/mainwindow.h"
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.4.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+namespace {
+struct qt_meta_stringdata_MainWindow_t {
+    uint offsetsAndSizes[42];
+    char stringdata0[11];
+    char stringdata1[24];
+    char stringdata2[1];
+    char stringdata3[24];
+    char stringdata4[25];
+    char stringdata5[24];
+    char stringdata6[24];
+    char stringdata7[25];
+    char stringdata8[25];
+    char stringdata9[25];
+    char stringdata10[25];
+    char stringdata11[25];
+    char stringdata12[24];
+    char stringdata13[25];
+    char stringdata14[25];
+    char stringdata15[25];
+    char stringdata16[22];
+    char stringdata17[25];
+    char stringdata18[25];
+    char stringdata19[25];
+    char stringdata20[24];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_MainWindow_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 23),  // "on_pushButton_1_clicked"
+        QT_MOC_LITERAL(35, 0),  // ""
+        QT_MOC_LITERAL(36, 23),  // "on_pushButton_2_clicked"
+        QT_MOC_LITERAL(60, 24),  // "on_pushButton_23_clicked"
+        QT_MOC_LITERAL(85, 23),  // "on_pushButton_3_clicked"
+        QT_MOC_LITERAL(109, 23),  // "on_pushButton_4_clicked"
+        QT_MOC_LITERAL(133, 24),  // "on_pushButton_20_clicked"
+        QT_MOC_LITERAL(158, 24),  // "on_pushButton_22_clicked"
+        QT_MOC_LITERAL(183, 24),  // "on_pushButton_21_clicked"
+        QT_MOC_LITERAL(208, 24),  // "on_pushButton_24_clicked"
+        QT_MOC_LITERAL(233, 24),  // "on_pushButton_25_clicked"
+        QT_MOC_LITERAL(258, 23),  // "on_pushButton_5_clicked"
+        QT_MOC_LITERAL(282, 24),  // "on_pushButton_26_clicked"
+        QT_MOC_LITERAL(307, 24),  // "on_pushButton_28_clicked"
+        QT_MOC_LITERAL(332, 24),  // "on_pushButton_27_clicked"
+        QT_MOC_LITERAL(357, 21),  // "on_pushButton_clicked"
+        QT_MOC_LITERAL(379, 24),  // "on_pushButton_18_clicked"
+        QT_MOC_LITERAL(404, 24),  // "on_pushButton_15_clicked"
+        QT_MOC_LITERAL(429, 24),  // "on_pushButton_13_clicked"
+        QT_MOC_LITERAL(454, 23)   // "on_pushButton_6_clicked"
+    },
+    "MainWindow",
+    "on_pushButton_1_clicked",
+    "",
+    "on_pushButton_2_clicked",
+    "on_pushButton_23_clicked",
+    "on_pushButton_3_clicked",
+    "on_pushButton_4_clicked",
+    "on_pushButton_20_clicked",
+    "on_pushButton_22_clicked",
+    "on_pushButton_21_clicked",
+    "on_pushButton_24_clicked",
+    "on_pushButton_25_clicked",
+    "on_pushButton_5_clicked",
+    "on_pushButton_26_clicked",
+    "on_pushButton_28_clicked",
+    "on_pushButton_27_clicked",
+    "on_pushButton_clicked",
+    "on_pushButton_18_clicked",
+    "on_pushButton_15_clicked",
+    "on_pushButton_13_clicked",
+    "on_pushButton_6_clicked"
+};
+#undef QT_MOC_LITERAL
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      19,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  128,    2, 0x08,    1 /* Private */,
+       3,    0,  129,    2, 0x08,    2 /* Private */,
+       4,    0,  130,    2, 0x08,    3 /* Private */,
+       5,    0,  131,    2, 0x08,    4 /* Private */,
+       6,    0,  132,    2, 0x08,    5 /* Private */,
+       7,    0,  133,    2, 0x08,    6 /* Private */,
+       8,    0,  134,    2, 0x08,    7 /* Private */,
+       9,    0,  135,    2, 0x08,    8 /* Private */,
+      10,    0,  136,    2, 0x08,    9 /* Private */,
+      11,    0,  137,    2, 0x08,   10 /* Private */,
+      12,    0,  138,    2, 0x08,   11 /* Private */,
+      13,    0,  139,    2, 0x08,   12 /* Private */,
+      14,    0,  140,    2, 0x08,   13 /* Private */,
+      15,    0,  141,    2, 0x08,   14 /* Private */,
+      16,    0,  142,    2, 0x08,   15 /* Private */,
+      17,    0,  143,    2, 0x08,   16 /* Private */,
+      18,    0,  144,    2, 0x08,   17 /* Private */,
+      19,    0,  145,    2, 0x08,   18 /* Private */,
+      20,    0,  146,    2, 0x08,   19 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.offsetsAndSizes,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'on_pushButton_1_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_23_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_3_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_4_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_20_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_22_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_21_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_24_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_25_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_5_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_26_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_28_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_27_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_18_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_15_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_13_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_6_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_pushButton_1_clicked(); break;
+        case 1: _t->on_pushButton_2_clicked(); break;
+        case 2: _t->on_pushButton_23_clicked(); break;
+        case 3: _t->on_pushButton_3_clicked(); break;
+        case 4: _t->on_pushButton_4_clicked(); break;
+        case 5: _t->on_pushButton_20_clicked(); break;
+        case 6: _t->on_pushButton_22_clicked(); break;
+        case 7: _t->on_pushButton_21_clicked(); break;
+        case 8: _t->on_pushButton_24_clicked(); break;
+        case 9: _t->on_pushButton_25_clicked(); break;
+        case 10: _t->on_pushButton_5_clicked(); break;
+        case 11: _t->on_pushButton_26_clicked(); break;
+        case 12: _t->on_pushButton_28_clicked(); break;
+        case 13: _t->on_pushButton_27_clicked(); break;
+        case 14: _t->on_pushButton_clicked(); break;
+        case 15: _t->on_pushButton_18_clicked(); break;
+        case 16: _t->on_pushButton_15_clicked(); break;
+        case 17: _t->on_pushButton_13_clicked(); break;
+        case 18: _t->on_pushButton_6_clicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 19)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 19;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 19)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 19;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
